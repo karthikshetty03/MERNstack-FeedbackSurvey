f7f1e75e0c326955668d03960378e0ac6e58b37b
Karthik Shetty <ks03mh04@gmail.com>
2020-03-12 20:08:23 +0530

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'); //commonJS modules
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({ hi : 'there'});
+    res.send({ bye : 'buddy'});
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
