ca6f990f16f1809e272502e58f175f5e4f180306
Karthik Shetty <ks03mh04@gmail.com>
2020-04-19 19:23:19 +0530

    Some changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,9 +5,7 @@ import { connect } from "react-redux";
 import * as actions from "../actions";
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
-import SurveyNew from './surveys/SurveyNew';
-
-
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
   componentDidMount() {
